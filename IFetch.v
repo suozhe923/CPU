c5aa47315961d1220d3a29a8887f128f62a0e497
suozhe923 <12312125@mail.sustech.edu.cn>
2025-05-11 22:16:55 +0800

    Update IFetch.v

--- IFetch.v
+++ IFetch.v
@@ -1,32 +1,19 @@
-`timescale 1ns / 1ps
-
 module IFetch(
-    input clk,
-    input rst,
-    input branch,
-    input zero,
-    input [31:0] imm32,
-    output [31:0] inst
-);
+    input clk, rst, branch, zero,
+    input[31:0] imm32,
+    output[31:0] inst
+    );
 
-reg [31:0] pc;
-wire [13:0] rom = pc[15:2];
+    reg[31:0] pc;
+    prgrom urom(.clka(clk), .addra(pc[15:2]), .douta(inst));
 
-prgrom urom (
-    .clka(clk),
-    .addra(rom),
-    .douta(inst)
-);
-
-always @(negedge clk) begin
-    if (~rst)
-        pc <= 32'h0000_0000;
-    else begin
-        case ({branch, zero})
-            2'b11: pc <= pc + imm32;
-            default: pc <= pc + 4;
-        endcase
+    always@ (negedge clk or negedge rst) begin
+        if(!rst) //initialize
+            pc<= 32'b0;
+        else if(branch & zero) //branch
+            pc<=pc+imm32;
+        else //Sequential execution
+            pc<=pc+4;
     end
-end
 
 endmodule
