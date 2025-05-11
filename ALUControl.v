8e72e6d39647c5f5308930cd48a9fc41a664ec76
suozhe923 <12312125@mail.sustech.edu.cn>
2025-05-11 23:07:32 +0800

    Update ALUControl.v
    rename regData2 to rs2Data

--- ALUControl.v
+++ ALUControl.v
@@ -1,33 +1,11 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2025/05/11 22:56:08
-// Design Name: 
-// Module Name: ALUControl
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module ALUControl(
     input [1:0] ALUSrc,
-    input [31:0] regData2, imm, pc,
+    input [31:0] rs2Data, imm, pc,
     output reg [31:0] op2
 );
 always @(*) begin
     case(ALUSrc)
-        0: op2 = regData2;
+        0: op2 = rs2Data;
         1: op2 = imm;
         2: op2 = pc;
         default: op2 = 0;
