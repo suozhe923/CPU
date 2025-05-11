32bc3a8139ebc643362025dcfcd930ef5d4dce3d
suozhe923 <12312125@mail.sustech.edu.cn>
2025-05-11 23:17:22 +0800

    Update IFetch.v
    add output pc

--- IFetch.v
+++ IFetch.v
@@ -1,10 +1,10 @@
 module IFetch(
     input clk, rst, branch, zero,
     input[31:0] imm32,
-    output[31:0] inst
+    output[31:0] inst,
+    output reg [31:0] pc
     );
 
-    reg[31:0] pc;
     prgrom urom(.clka(clk), .addra(pc[15:2]), .douta(inst));
 
     always@ (negedge clk or negedge rst) begin
