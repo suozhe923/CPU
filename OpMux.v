8aa3c3c6b232c684decf33e9e36b5d91383032ec
suozhe923 <12312125@mail.sustech.edu.cn>
2025-05-13 01:13:14 +0800

    Update OpMux.v (imm->imm32)

--- OpMux.v
+++ OpMux.v
@@ -1,12 +1,12 @@
 module OpMux(
     input [1:0] ALUSrc,
-    input [31:0] rs2Data, imm, pc,
+    input [31:0] rs2Data, imm32, pc,
     output reg [31:0] op2
 );
 always @(*) begin
     case(ALUSrc)
         0: op2 = rs2Data;
-        1: op2 = imm;
+        1: op2 = imm32;
         2: op2 = pc;
         default: op2 = 0;
     endcase
